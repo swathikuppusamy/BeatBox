a4f52838d861a41de526f78e8174241373b0c5b1
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-02-05 18:59:54 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+import { Button } from "./components/ui/button"
+
+
+const App = () => {
+  return (
+    <>
+      <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
+  )
+}
+
+export default App
