d5ddc23b251a13029e779936762dddac00141318
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-02-13 22:27:12 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,8 @@ import path from 'path'
 import cors from 'cors'
 import { createServer } from 'http'
 import { initializeSocket } from './lib/socket.js'
-
+import cron from 'node-cron'
+import fs from 'fs'
 import { connectdb } from './lib/db.js'
 import userRoutes from './routes/user.route.js'
 import adminRoutes from './routes/admin.route.js'
@@ -47,12 +48,37 @@ app.use(fileUpload({
   }
 }))
 
+
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
+
+
 app.use('/api/users',userRoutes)
 app.use('/api/admin',adminRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/songs',songRoutes)
 app.use('/api/albums',albumRoutes)
 app.use('/api/stats',statRoutes)
+
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+	});
+}
 
 
 app.use((error,req,res,next)=>{
