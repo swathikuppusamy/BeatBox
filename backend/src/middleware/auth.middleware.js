a5210491d8fcf7378c246227b205492a36f25df3
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-02-14 12:07:46 +0530

    auth change

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,17 +1,17 @@
 import { clerkClient } from "@clerk/express";
 
-// export const protectRoute = async (req, res, next) => {
-// 	if (!req.auth.userId) {
-// 		return res.status(401).json({ message: "Unauthorized - you must be logged in" });
-// 	}
-// 	next();
-// };
 export const protectRoute = async (req, res, next) => {
-	if (!req.auth || !req.auth.userId) {
+	if (!req.auth.userId) {
 		return res.status(401).json({ message: "Unauthorized - you must be logged in" });
 	}
 	next();
 };
+// export const protectRoute = async (req, res, next) => {
+// 	if (!req.auth || !req.auth.userId) {
+// 		return res.status(401).json({ message: "Unauthorized - you must be logged in" });
+// 	}
+// 	next();
+// };
 
 
 export const requireAdmin = async (req, res, next) => {
