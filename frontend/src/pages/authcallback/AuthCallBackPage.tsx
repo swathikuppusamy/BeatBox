27e332184a8a04d473c2aade6fb4e35515f168ce
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-02-16 10:20:24 +0530

    rectified /auth

--- frontend/src/pages/authcallback/AuthCallBackPage.tsx
+++ frontend/src/pages/authcallback/AuthCallBackPage.tsx
@@ -17,7 +17,7 @@ const AuthCallbackPage = () => {
 			try {
 				syncAttempted.current = true;
 
-				await axiosInstance.post("/auth/callback", {
+				await axiosInstance.post("/auth", {
 					id: user.id,
 					firstName: user.firstName,
 					lastName: user.lastName,
