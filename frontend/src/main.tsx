9478485940a4b16ea0dcdb8460b81650920b8e36
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-02-16 11:28:49 +0530

    removed /

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
      <AuthProvider>
      <BrowserRouter>
      <App />
